0a996da55dba18eeec1f7b4fa666e1fd44c27154
keepe <vosetru@gmail.com>
2021-04-11 00:23:20 +0300

    version_0.2

---
+++ Bio.py
@@ -0,0 +1,11 @@
+#ввод данных
+dnk = list(map(str, input('Введите 5 алелей ДНК (через) - ').split('-')))
+print(dnk[0])
+
+#одна алель
+A1 = dnk[0]
+print(A1)
+
+#нуклеотиды
+sim = list(A1)
+print(sim[0])
